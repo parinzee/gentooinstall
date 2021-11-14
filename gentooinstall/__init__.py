90da65c73be48b40fb248dd172140b326e8de04b
Parinz <parinzee@protonmail.com>
2021-11-14 11:11:38 +0700

    Added argument parsing and storage module

--- gentooinstall/__init__.py
+++ gentooinstall/__init__.py
@@ -1,4 +1,23 @@
 # pylint: disable=missing-module-docstring
+import argparse
+
+from .lib.storage import storage
 from .script import *
 
 __version__ = "0.1.0"
+
+# Initialize argument parser
+parser = argparse.ArgumentParser(
+    prog="gentooinstall",
+    description="The program aimed to help both beginners and experienced users install gentoo.",
+)
+
+# no-info option
+parser.add_argument(
+    "--no-info",
+    action="store_true",
+    help="Disables explanations for steps from the Gentoo Handbook. NOT RECCOMENDED FOR BEGINNERS",
+)
+
+# Keep the args in our storage
+storage["args"] = parser.parse_args()
---
+++ gentooinstall/lib/storage.py
@@ -0,0 +1,19 @@
+"""
+Simple module for storing arguments that can be accessed throughout the program
+"""
+from argparse import Namespace
+from typing import TypedDict
+
+
+# Define schema for the Storage dict
+class Storage(TypedDict):
+    """
+    Schema for the storage dictionary
+    """
+
+    args: Namespace
+    mountpoint: str
+    drives: str
+
+
+storage: Storage = {"args": Namespace(), "mountpoint": "/mnt/gentoo", "drives": ""}
